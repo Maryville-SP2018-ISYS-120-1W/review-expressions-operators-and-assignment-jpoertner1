ce85abb9ae951f02dfcaea8a7ba13e4976395794
jpoertner <jordanpoertner@gmail.com>
2018-01-26 15:12:22 -0500

    Changes

--- ExpressionsAndAssignmentReview.playground/Contents.swift
+++ ExpressionsAndAssignmentReview.playground/Contents.swift
@@ -15,3 +15,28 @@
  
 */
 
+
+
+/*
+ You are going shopping for some random items.  First create a variable called cashAmt of type Double and set it to 250.  Next, create a variable called groceriesObtained of type Int and set it to 0.
+        You pick up food ingredients for $75.99, so use a compound operator to decrease your cashAmt by this.  Increase your groceries obtained by 8.
+        Now you have hit the electronics aisle and you remember your kid wanted some amiibo thing.  His Birthday is coming up so you decide to pick it up early.  You use $24.99 of your cashAmt to pick it up.  Use a compound operator for this.
+        After checking out of the store, you place the groceries in the car.  Increase your groceriesObtained by 1 then print it followed by printing your remaining cashAmt.
+
+ */
+
+
+/*
+    You have arrived at the second store.
+        Along the way, you stopped to pick up some ice cream for your family.  This used another $9 of your cashAmt and increased your groceriesObtained by 1.  Change the values accordingly.
+        As you step out of the car, you are moving some things around in the back of your vehicle to make room for more groceries.  At this time, you are mugged by a kid and he steals your newly obtained ice cream.  Decrease your groceriesObtained by 1 accordingly.
+        Now create a constant groceriesLost and set it to 1.
+        Saddened by the loss of your dessert, you decided to just go home as you were too scared after what just happened.
+ */
+
+
+/*
+    As you pull up into the driveway, your neighbor comes over laughing and is jumping up and down while shouting about how he won the lottery.  With his newfound money, he declares he is going to double the current amount of cash you have on you.  However, he does not want to deal with change and tells you to exclude any pennies in the calculation, but he will instead give you an extra $2 to make up for it.
+        In one line, use a compound operator to convert your cashAmt to an Int, double it, then add 2 to the result.
+        Print your cashAmt, then your groceriesObtained, and finally your groceriesLost.
+*/
